e0d22602beed6ec2f858b3d6f13ef02c43626719
yuka.tokoro <yuka.tokoro@CS-475.CSRN.local>
2018-06-05 16:35:26 +0900

    コナン！

--- src/bean/ConanBean.java
+++ src/bean/ConanBean.java
@@ -1,82 +1,45 @@
 package bean;
 
 public class ConanBean {
-    int thisMonth;
-    int nextMonth;
-    int total;
-    String goodCategory;
-    int goodDifference;
-    String badCategory;
-    int badDifference;
+    String categoryName;
+    int goal;
+    int spending;
+    int difference;
 
-    public int getThisMonth() {
-        return thisMonth;
+    public String getCategoryName() {
+        return categoryName;
     }
 
-    public void setThisMonth(int thisMonth) {
-        this.thisMonth = thisMonth;
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
     }
 
-    public int getNextMonth() {
-        return nextMonth;
+    public int getGoal() {
+        return goal;
     }
 
-    public void setNextMonth(int nextMonth) {
-        this.nextMonth = nextMonth;
+    public void setGoal(int goal) {
+        this.goal = goal;
     }
 
-    public int getTotal() {
-        return total;
+    public int getSpending() {
+        return spending;
     }
 
-    public void setTotal(int total) {
-        this.total = total;
+    public void setSpending(int spending) {
+        this.spending = spending;
     }
 
-    public String getGoodCategory() {
-        return goodCategory;
+    public int getDifference() {
+        return difference;
     }
 
-    public void setGoodCategory(String goodCategory) {
-        this.goodCategory = goodCategory;
-    }
-
-    public int getGoodDifference() {
-        return goodDifference;
-    }
-
-    public void setGoodDifference(int googDifference) {
-        this.goodDifference = googDifference;
-    }
-
-    public String getBadCategory() {
-        return badCategory;
-    }
-
-    public void setBadCategory(String badCategory) {
-        this.badCategory = badCategory;
-    }
-
-    public int getBadDifference() {
-        return badDifference;
-    }
-
-    public void setBadDifference(int badDifference) {
-        this.badDifference = badDifference;
+    public void setDifference(int difference) {
+        this.difference = difference;
     }
 
     @Override
     public String toString() {
-<<<<<<< HEAD
         return categoryName + "の目標金額は" + goal + "円、\t" + "支出合計は" + spending + "円、\t差額は" + difference + "円\tだよ。";
-=======
-        return thisMonth + "月は目標達成だね！おめでとう(^_^)/♪<br>" +
-                "君のおかげで犯人を逮捕することができたよ！<br>" +
-                "目標金額より" + total + "円節約できたね。えらい！！<br>" +
-                "一番節約したのは" + goodCategory + "で、" + goodDifference + "円も節約できたね！<br>" +
-                "でも" + badCategory + "は" + badDifference + "円オーバーしちゃったね･･･。気をつけよう！<br>" +
-                "この調子で" + nextMonth + "月もがんばってね！";
->>>>>>> branch 'master' of https://github.com/CentralsoftTrain2018/kakeibo
     }
-
 }
--- src/servlet/ConanServlet.java
+++ src/servlet/ConanServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.ConanBean;
+import bean.ConanListBean;
 
 @WebServlet("/ConanServlet")
 public class ConanServlet extends HttpServlet {
@@ -21,9 +24,7 @@ public class ConanServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        ConanBean bean = new ConanBean();
 
-<<<<<<< HEAD
         //カテゴリ数を取得（SQLでやる）
         int categorySu = 3;
 
@@ -58,25 +59,11 @@ public class ConanServlet extends HttpServlet {
         ConanListBean bean = new ConanListBean();
         bean.setList(list);
         //何とかする
-=======
-        //計算結果と表示するメッセージを入れ物（bean)にセットする
->>>>>>> branch 'master' of https://github.com/CentralsoftTrain2018/kakeibo
         bean.setThisMonth(5);
-<<<<<<< HEAD
         bean.setTotalGoal(sumg);
         bean.setTotalSpending(sums);
         bean.setTotalDifference(sumg - sums);
 
-=======
-        bean.setNextMonth(6);
-        bean.setTotal(20000);
-        bean.setGoodCategory("食費");
-        bean.setGoodDifference(500);
-        bean.setBadCategory("交際費");
-        bean.setBadDifference(3000);
-
-        //beanをリクエストにセット キー名は「bean」とする
->>>>>>> branch 'master' of https://github.com/CentralsoftTrain2018/kakeibo
         request.setAttribute("bean", bean);
 
         //JSPに遷移する
