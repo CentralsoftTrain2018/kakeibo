091e9679c8149295ef9d40e0b22c0a64b2154161
Kazuya-kazuya <kazuya.nakayama@central-soft.co.jp>
2018-06-05 15:29:03 +0900

    marge

--- WebContent/Conan.jsp
+++ WebContent/Conan.jsp
@@ -1,12 +1,16 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<jsp:useBean id="bean" class="bean.ConanBean" scope="request" />
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>コナン</title>
+<link rel="stylesheet" type="text/css" href="css/balloon.css">
+<title>コナンくんからのアドバイス</title>
 </head>
 <body>
-コナンからのアドバイス
+<img src="image/conan.png" style="float:left;">
+<div class="balloon">
+<%=bean.toString() %>
+</div>
 </body>
 </html>
--- src/service/Service.java
+++ src/service/Service.java
@@ -7,29 +7,18 @@ import vo.ExpenseVo;
 
 public class Service {
     public static void addExpense(int kingaku, String categoryName, String expenseName, String userId) {
-        try {
-            Category.valueOf(categoryName);
-        }
-        catch(IllegalArgumentException e) {
-            e.printStackTrace();
-        }
 
         ExpenseVo ev = new ExpenseVo();
         ev.setExpenseKingaku(kingaku);
         ev.setCategoryName(categoryName);
         ev.setExpenseName(expenseName);
         ev.setUserId(userId);
+        Date expenseDate = new Date(new java.util.Date().getTime());
+        ev.setExpenseDate(expenseDate);
         DBManager.addExpense(ev);
     }
 
     public static void updateExpense(int expenseId, int kingaku, String categoryName, String expenseName, Date expenseDate, String userId) {
-        try {
-            Category.valueOf(categoryName);
-        }
-        catch(IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-
         ExpenseVo ev = new ExpenseVo();
         ev.setExpenseId(expenseId);
         ev.setExpenseKingaku(kingaku);
