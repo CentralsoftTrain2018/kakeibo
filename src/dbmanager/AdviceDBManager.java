3e3de3dde282e9af0c7ae80b526d4d5c37fa924a
NaoIzw <38120316+NaoIzw@users.noreply.github.com>
2018-06-07 14:01:50 +0900

    アドバイスDBマネージャー、アドバイスサービスに自白用のメソッドを追加

--- src/dbmanager/AdviceDBManager.java
+++ src/dbmanager/AdviceDBManager.java
@@ -5,19 +5,38 @@ import java.sql.SQLException;
 import java.util.List;
 
 import dao.ConanDao;
-import vo.ConanVo;
+import dao.JihakuDao;
+import vo.AdviceVo;
 
 public class AdviceDBManager {
     //アドバイス（コナン）に必要なやつを取ってくる
-    public static List<ConanVo> selectConanAdvice() {
+    public static List<AdviceVo> selectConanAdvice() {
         try
         (
             Connection con = PoolConnection.getConnection();
         )
         {
             ConanDao cdao = new ConanDao(con);
-            List<ConanVo> list = cdao.advice();
+            List<AdviceVo> list = cdao.advice();
            return list;
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    //アドバイス(自白)に必要なデータを取得する
+    public static List<AdviceVo> selectJihakuAdvice() {
+        try
+        (
+            Connection con = PoolConnection.getConnection();
+        )
+        {
+            JihakuDao jdao = new JihakuDao();
+            List<AdviceVo> list = new jdao.jihakuAdvice();
+            return list;
         }
         catch(SQLException e)
         {
--- src/service/AdviceService.java
+++ src/service/AdviceService.java
@@ -8,58 +8,52 @@ import bean.ConanListBean;
 import bean.JihakuBean;
 import bean.JihakuListBean;
 import dbmanager.AdviceDBManager;
-import vo.ConanVo;
+import vo.AdviceVo;
 
 public class AdviceService {
 
+    //AdviceVo型のListをConanBean型のListに変換
     public JihakuListBean jihaku() {
-        JihakuListBean jihakulist= new JihakuListBean();
+        List<AdviceVo> resultList = AdviceDBManager.selectJihakuAdvice();
+        JihakuListBean jlb= new JihakuListBean();
 
-        JihakuBean bean = new JihakuBean();
-        int sisyutu = 10000;
-        int mokuhyou = 5000;
-        String categoryname = ("雑費");
         //計算結果と表示するメッセージを入れ物（bean)にセットする
-        bean.setCategoryname(categoryname);
-        bean.setExcess(sisyutu,mokuhyou);
+        for(AdviceVo av : resultList)
+        {
+            JihakuBean jb = new JihakuBean();
+            jb.setCategoryname( av.getCategoryName());
+            jb.setExcess( av.getDifference() );
 
-        JihakuBean bean1 = new JihakuBean();
-        int sisyutu1 = 10000;
-        int mokuhyou1= 5000;
-        String categoryname1 = ("食費");
-        //計算結果と表示するメッセージを入れ物（bean)にセットする
-        bean1.setCategoryname(categoryname1);
-        bean1.setExcess(sisyutu1,mokuhyou1);
+            jlb.getJihakulist().add(jb);
+        }
 
-        jihakulist.getJihakulist().add(bean);
-        jihakulist.getJihakulist().add(bean1);
-        return jihakulist;
+        return jlb;
     }
 
 //---------------------------コナン機能---------------------------------------------------------
-    //ConanVo型のListをConanBean型のListに変換
+    //AdviceVo型のListをConanBean型のListに変換
     public static ConanListBean selectConanAdvice() {
-        List<ConanVo> resultList = AdviceDBManager.selectConanAdvice();
+        List<AdviceVo> resultList = AdviceDBManager.selectConanAdvice();
         ConanListBean clb = new ConanListBean();
 
         int totalGoal = 0;		//月の目標
         int totalSpending = 0;	//月の支出合計
         int totalDifference = 0;	//月の目標ー支出
         List<ConanBean> list = new ArrayList<ConanBean>();
-        for( ConanVo cv: resultList )
+        for( AdviceVo av: resultList )
         {
             ConanBean cb = new ConanBean();
-            cb.setCategoryName(cv.getCategoryName());
+            cb.setCategoryName(av.getCategoryName());
 
-            int spanding = cv.getSumSpending();
+            int spanding = av.getSumSpending();
             totalSpending += spanding;
             cb.setSumSpending(spanding);
 
-            int goal = cv.getMokuhyouKingaku();
+            int goal = av.getMokuhyouKingaku();
             totalGoal += goal;
             cb.setMokuhyouKingaku(goal);
 
-            int difference = cv.getDifference();
+            int difference = av.getDifference();
             totalDifference += difference;
             cb.setDifference(difference);
 
