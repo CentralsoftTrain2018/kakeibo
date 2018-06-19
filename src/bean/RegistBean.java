96e8d5091dea05c0d0e1062440ddadf85fdbff8d
tamagodaisukichan <yuka.tokoro@central-soft.co.jp>
2018-06-19 10:05:45 +0900

    add
    service, manager追加

--- src/bean/RegistBean.java
+++ src/bean/RegistBean.java
@@ -2,5 +2,54 @@ package bean;
 
 public class RegistBean
 {
+    private String userId;
+    private String password;
+    private String mail;
+    private int income;
 
+    public String getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId( String userId )
+    {
+        this.userId = userId;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword( String password )
+    {
+        this.password = password;
+    }
+
+    public String getMail()
+    {
+        return mail;
+    }
+
+    public void setMail( String mail )
+    {
+        this.mail = mail;
+    }
+
+    public int getIncome()
+    {
+        return income;
+    }
+
+    public void setIncome( int income )
+    {
+        this.income = income;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "userId: " + userId + ", password: " + password + ", mail: " + mail + ", income: " + income;
+    }
 }
---
+++ src/service/UserDataSevice.java
@@ -0,0 +1,12 @@
+package service;
+
+import bean.RegistBean;
+
+public class UserDataSevice
+{
+    public static void passRegistDara(RegistBean rb)
+    {
+        System.out.println( "passRegistDara" );
+
+    }
+}
