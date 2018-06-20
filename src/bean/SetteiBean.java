3a9b0f3f0b47a8c42a40bdfe69dbdecaf58f0df7
takeshi.ino <takeshi.ino@CS-472.CSRN.local>
2018-06-20 11:03:45 +0900

    add
    settei.js

--- src/bean/SetteiBean.java
+++ src/bean/SetteiBean.java
@@ -5,17 +5,23 @@ import java.util.List;
 import vo.SetteiVo;
 
 public class SetteiBean {
-    private String categoryname;
+    //カテゴリー毎の目標金額、カテゴリー名、カテゴリーIDを持つリスト
     private List<SetteiVo> categoryMokuhyouList;
+
+    //現在の収入を表示するための変数
     private int syunyuu;
+
+    //現在の項目ごとの目標金額の合計を表示するための変数
+    private int mokuhyougoukei;
+
     private String userId;
 
-    public String getCategoryname() {
-        return categoryname;
-    }
-    public void setCategoryname(String categoryname) {
-        this.categoryname = categoryname;
-    }
+    //現在のパスワードを表示するための変数
+    private String userPass;
+
+    //画面遷移の際前回表示していた画面が何かを記録するための変数
+    private String disp;
+
     public List<SetteiVo> getCategoryMokuhyouList() {
         return categoryMokuhyouList;
     }
@@ -34,6 +40,24 @@ public class SetteiBean {
     }
     public void setSyunyuu(int syunyuu) {
         this.syunyuu = syunyuu;
+    }
+    public int getMokuhyougoukei() {
+        return mokuhyougoukei;
+    }
+    public void setMokuhyougoukei(int mokuhyougoukei) {
+        this.mokuhyougoukei = mokuhyougoukei;
+    }
+    public String getUserPass() {
+        return userPass;
+    }
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+    public String getDisp() {
+        return disp;
+    }
+    public void setDisp(String disp) {
+        this.disp = disp;
     }
 
 
